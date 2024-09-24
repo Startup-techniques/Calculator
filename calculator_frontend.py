b3d1bfbbe0cf30e021bbd41bdceee12e200a893b
AlexandrKhvan <alexandrkhvan2005@gmail.com>
2024-09-24 20:46:10 +0300

    improvements to frontend and html

--- calculator_frontend.py
+++ calculator_frontend.py
@@ -4,22 +4,23 @@ import requests
 
 app = Flask(__name__)
 
-history = []
+history_list = []
+current_result = None
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    global history_list, current_result
     if request.method == 'POST':
         if request.form.get('action') == 'history':
             result = requests.get("http://127.0.0.1:3001/history").content.decode('ascii')
-            # TODO: convert result to list of strings
-            # then set glogal variable history with this list
-            # this list will be displayed
-
+            values = [x[1:-1] for x in result[1:-2].split(',')]
+            history_list = values
         else:
             expr = request.form.get('expression')
             result = requests.post("http://127.0.0.1:3001/submit", json={'exp': expr}).content.decode('ascii')
-            # TODO: show result some way
-    return render_template('index.html', title='Home', history=history)
+            current_result = result
+            history_list.insert(0, f'{expr} = {result}')
+    return render_template('index.html', title='Home', history=history_list, result=current_result)
 
 
 if __name__ == '__main__':
